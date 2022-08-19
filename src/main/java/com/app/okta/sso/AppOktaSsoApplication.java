1224edf59083703cfeabac5f44d6192b6f28637b
Panthi <anish.panthi41@gmail.com>
2022-08-19 09:54:15 -0500

    Initial Commit

---
+++ src/main/java/com/app/okta/sso/AppOktaSsoApplication.java
@@ -0,0 +1,13 @@
+package com.app.okta.sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppOktaSsoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppOktaSsoApplication.class, args);
+    }
+
+}
